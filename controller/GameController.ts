5b889766f12beff8224a6fc46dcdf232a5fbcc1c
mandy-lam <mandylxdd@gmail.com>
2023-05-31 11:54:43 +0800

    waiting update

--- controller/GameController.ts
+++ controller/GameController.ts
@@ -1,8 +1,12 @@
 import { parseFormData } from "../formidable";
 import { Request, Response } from "express";
-import { db } from "../db";
+// import { db } from "../db";
 
 import { errorHandler } from "../errorHandler";
+import { IGameController } from "../routes/Routes";
+
+import jsonfile from "jsonfile";
+import { type } from "os";
 
 type Game = {
     gameName:string,
@@ -11,19 +15,35 @@ type Game = {
     gameCover?:string,
 }
 
-export class GameController{
+export class GameController implements IGameController{
+    
     async addGameList(req:Request,res:Response):Promise<void>{
+
+        
+
         try {
             
             let form = await parseFormData(req) as Game; 
-            // if(form.password !== form.confirmPassword){
-            //     throw new Error('password not match!')
-            // }
-            let gameData = {...form}
 
-            await db.query(`INSERT INTO game (name, game_type, like_count, description, create_users_id, game_cover) VALUES ($1,$2,$3,$4,$5,$6)`,
-            [gameData.gameName, gameData.game_type, 0, gameData.description, req.session.userId, gameData.gameCover])
+            let gameData = {...form}
+            console.log(console.log(`ts game form result: ${gameData}`))
             
+            // let gamesJSON: Array<Game> = await    jsonfile.readFile("games.json")
+            // gamesJSON.push({
+            //     gameName: gameData.gameName,
+            //     game_type: gameData.game_type,
+            //     description: gameData.description,
+            //     gameCover: gameData.gameCover?
+            // });
+            
+            // await jsonfile.writeFile("games.json", gameData, {spaces: 4})
+            // res.redirect("/gameForm.html")
+            
+            // await db.query(`INSERT INTO game (name, game_type, like_count, description, create_users_id, game_cover) VALUES ($1,$2,$3,$4,$5,$6)`,
+            // [gameData.gameName, gameData.game_type, 0, gameData.description, req.session.userId, gameData.gameCover])
+            
+             
+
             res.json({isError:false,errMess:null,data:gameData});
 
         } catch (error) {
@@ -31,7 +51,38 @@ export class GameController{
             errorHandler({status:error.status,route:req.path,errMess:error.message})
             res.json({isError:true,errMess:error.message})
         }
-
-
     }
+
+    async editGameList(req:Request,res:Response):Promise<void>{
+        try {
+            
+        } catch (error) {
+            errorHandler({status:error.status,route:req.path,errMess:error.message})
+            res.json({isError:true,errMess:error.message})
+        }
+    }
+
+    async deleteGameList(req:Request,res:Response):Promise<void>{
+        try {
+            
+        } catch (error) {
+            errorHandler({status:error.status,route:req.path,errMess:error.message})
+            res.json({isError:true,errMess:error.message})
+        }
+    }
+
+    async getGameList(req:Request,res:Response):Promise<void> {
+        try {
+            // let {rows} = await db.query(`SELECT id,name,game_type,like_count,create_at, description, game_cover FROM game WHERE id = $1`,[gameData.game.id]);
+            
+            // res.json({isError:false,errMess:"",data:rows[0]})    
+            
+        } catch (error) {
+            errorHandler({status:error.status,route:req.path,errMess:error.message})
+            res.json({isError:true,errMess:error.message})
+        }
+    }
+
+
+    
 }
--- public/gameForm.js
+++ public/gameForm.js
@@ -4,45 +4,45 @@
 // function renderGameFormModal(){
   
 
-  let form = document.querySelector('#gameForm')
-   form.addEventListener("submit", async function (event) {
-      event.preventDefault();
+let form = document.querySelector('#gameForm')
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
 
-      // Serialize the Form afterwards
-      
-      console.log(form)
-      const formData = new FormData(form);
-
-      // Submit FormData(), no need to add "Content-Type": "application/json"
-      const res = await fetch("game/gameList", {
-        method: "POST",
-        body: formData,
-      });
-
-      const result = await res.json(); // { success: true }
-      console.log(result)
-
-      if(json.isError){
-        await sweetAlert.fire({
-            icon: 'info',
-            title: json.errMess,
-            showConfirmButton: false,
-            timer: 1500
-          })
-          
-    }else{
-        await sweetAlert.fire({
-            icon: 'success',
-            title: 'Successfully Login',
-            showConfirmButton: false,
-            timer: 1500
-          })
-        }
-      
-      // const gameSection = document.querySelector(".gameSection")
-      // gameSection.innerHTML += result;
-
-
+  // Serialize the Form afterwards
   
+  console.log(form)
+  const formData = new FormData(form);
+
+  // Submit FormData(), no need to add "Content-Type": "application/json"
+  const res = await fetch("game/gameList", {
+    method: "POST",
+    body: formData,
   });
+
+  const result = await res.json(); // { success: true }
+  console.log(`js game form result: ${result}`)
+
+  if(json.isError){
+    await sweetAlert.fire({
+        icon: 'info',
+        title: json.errMess,
+        showConfirmButton: false,
+        timer: 1500
+  })
+      
+  }else{
+    await sweetAlert.fire({
+        icon: 'success',
+        title: 'Successfully Login',
+        showConfirmButton: false,
+        timer: 1500
+    })
+  }
+  
+  // const gameSection = document.querySelector(".gameSection")
+  // gameSection.innerHTML += result;
+
+
+
+});
 
