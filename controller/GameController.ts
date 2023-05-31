90520b666113d3e6dcce3dd3566bd9249d39dd2e
mandy-lam <mandylxdd@gmail.com>
2023-05-31 16:32:41 +0800

    able to add game to db, update game sql

--- controller/GameController.ts
+++ controller/GameController.ts
@@ -1,12 +1,12 @@
-import { parseFormData } from "../formidable";
+import { parseFormDataGame } from "../formidable";
 import { Request, Response } from "express";
-// import { db } from "../db";
+import { db } from "../db";
 
 import { errorHandler } from "../errorHandler";
 import { IGameController } from "../routes/Routes";
 
-import jsonfile from "jsonfile";
-import { type } from "os";
+// import jsonfile from "jsonfile";
+// import { type } from "os";
 
 type Game = {
     gameName:string,
@@ -19,27 +19,26 @@ export class GameController implements IGameController{
     
     async addGameList(req:Request,res:Response):Promise<void>{
 
-        
-
         try {
             
-            let form = await parseFormData(req) as Game; 
-
+            let form = await parseFormDataGame(req) as Game; 
+            console.log("25", form)
             let gameData = {...form}
-            console.log(console.log(`ts game form result: ${gameData}`))
+            // console.log(console.log(`ts game form result: ${gameData}`))
             
-            // await db.query(`INSERT INTO game (name, game_type, like_count, description, create_users_id, game_cover) VALUES ($1,$2,$3,$4,$5,$6)`,
-            // [gameData.gameName, gameData.game_type, 0, gameData.description, req.session.userId, gameData.gameCover])
+            await db.query(`INSERT INTO game (name, game_type, like_count, description, create_users_id, game_cover) VALUES ($1,$2,$3,$4,$5,$6)`,
+            [gameData.gameName, gameData.game_type, 0, gameData.description, req.session.userId, gameData.gameCover])
             
-             
 
-            res.json({isError:false,errMess:null,data:gameData});
+            res.json({isError:false,errMess:null,data:form});
 
         } catch (error) {
             
             errorHandler({status:error.status,route:req.path,errMess:error.message})
             res.json({isError:true,errMess:error.message})
         }
+
+        
     }
 
     async editGameList(req:Request,res:Response):Promise<void>{
@@ -62,7 +61,8 @@ export class GameController implements IGameController{
 
     async getGameList(req:Request,res:Response):Promise<void> {
         try {
-            // let {rows} = await db.query(`SELECT id,name,game_type,like_count,create_at, description, game_cover FROM game WHERE id = $1`,[gameData.game.id]);
+
+            // let {rows} = await db.query(`SELECT id,name,game_type,like_count,create_at, description, create_users_id, game_cover FROM game WHERE id = $1`,[]);
             
             // res.json({isError:false,errMess:"",data:rows[0]})    
             
--- public/gameForm.js
+++ public/gameForm.js
@@ -1,31 +1,28 @@
 // const { json } = require("stream/consumers");
 
 // working...
-// function renderGameFormModal(){
+
   
 
 let form = document.querySelector('#gameForm')
 form.addEventListener("submit", async function (event) {
   event.preventDefault();
-
-  // Serialize the Form afterwards
   
-  console.log(form)
+  // console.log(form)
   const formData = new FormData(form);
 
-  // Submit FormData(), no need to add "Content-Type": "application/json"
-  const res = await fetch("game/gameList", {
+  let res = await fetch("game/gameList", {
     method: "POST",
     body: formData,
   });
 
-  const result = await res.json(); // { success: true }
-  console.log(`js game form result: ${result}`)
+  let json = await res.json(); // { success: true }
+  console.log(json)
 
   if(json.isError){
     await sweetAlert.fire({
         icon: 'info',
-        title: json.errMess,
+        title: gameResult.errMess,
         showConfirmButton: false,
         timer: 1500
   })
@@ -33,14 +30,42 @@ form.addEventListener("submit", async function (event) {
   }else{
     await sweetAlert.fire({
         icon: 'success',
-        title: 'Successfully Login',
+        title: 'Successfully Post Game',
         showConfirmButton: false,
         timer: 1500
     })
   }
+  await form.reset();
+
+  // document.querySelector('#modalDismiss').click()
   
   // const gameSection = document.querySelector(".gameSection")
-  // gameSection.innerHTML += result;
+  // gameSection.innerHTML += `
+  //   <div class="gamebox card mb-3" style="max-width: 500px;">
+  //   <div class="row g-0">
+  //     <div class="col-md-4">
+  //       Game Cover
+  //       <img src="" class="img-fluid rounded-start gBoxCover" alt="...">
+  //     </div>
+  //     <div class="col-md-8">
+  //       <div class="card-body gBoxBody">
+  //         <div class="card-text gBoxType">Game Type</div>
+  //         <h4 class="card-title gBoxName">Game Name</h4>
+  //         <p class="card-text gBoxDescription">This is a wider card with supporting text below as a natural lead-in
+  //           to additional content. This content is a little bit longer.</p>
+  //         <div class="card-text">Created by:</div>
+  //         <div class="card-text">Create at</div>
+  //       </div>
+  //       <div class="card-body gBoxCount">
+  //         <i class="fa-regular fa-comment-dots"> Message: 100</i>
+  //         <i class="fa-regular fa-heart"> like: 100</i>
+  //       </div>
+
+  //     </div>
+  //   </div>
+  // </div>
+
+  // `;
 
 
 
