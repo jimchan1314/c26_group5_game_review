7b15b4775386724170500347b2124c8a68720c45
lwstr0410 <lwstr0410@gmail.com>
2023-06-01 18:53:26 +0800

    message form

--- controller/MessageController.ts
+++ controller/MessageController.ts
@@ -5,15 +5,31 @@ import { IMessageController } from "../routes/Routes";
 import { errorHandler } from "../errorHandler";
 
 type Message = {
-    message: string,
+    text: string,
     image: string,
-    game_id: number|string,
-    user_id: number|string
+    //game_id: number|string,
+    //user_id: number|string
 }
 
 export class MessageController implements IMessageController{
+    //need user login
     async addMessage(req:Request,res:Response):Promise<void> {
         try {
+            let form = await parseFormDataGame(req) as Message;
+            let message = {...form}
+
+            console.log(message)
+            
+            //await db.query(`INSERT INTO game_message (text,image,game_id,users_id) VALUES ($1,$2,$3,$4)`,
+            //[message.text,message.image,message.game_id,message.user_id])
+
+            let gameId = await db.query(`SELECT id from game limit 1`)
+            let userId = await db.query(`SELECT id from users limit 1`)
+
+            await db.query(`INSERT INTO game_message (text,image,game_id,users_id) VALUES ($1,$2,$3,$4)`,
+            [message.text,message.image,gameId,userId])
+            
+            res.json({isError:false,errMess:null,data:form});
 
         } catch (error) {
             errorHandler({status:error.status,route:req.path,errMess:error.message})
@@ -23,7 +39,10 @@ export class MessageController implements IMessageController{
 
     async editMessage(req:Request,res:Response):Promise<void> {
         try {
-
+            let messageID = req.params.id
+            let form = await parseFormDataGame(req) as Message
+            db.query(`UPDATE game_message SET text=$1 where id=$2`,[form.text,messageID])
+            res.json({isError:false,errMess:null,data:"edited successfully"});
         } catch (error) {
             errorHandler({status:error.status,route:req.path,errMess:error.message})
             res.json({isError:true,errMess:error.message})
@@ -32,16 +51,19 @@ export class MessageController implements IMessageController{
 
     async deleteMessage(req:Request,res:Response):Promise<void> {
         try {
-
+            let messageID = req.params.id
+            db.query(`DELETE game_message where id=$1`,[messageID])
         } catch (error) {
             errorHandler({status:error.status,route:req.path,errMess:error.message})
             res.json({isError:true,errMess:error.message})
         }
     }
 
+    //no need user login
     async getMessage(req:Request,res:Response):Promise<void> {
         try {
-
+            let messages = await db.query(`SELECT * from game_message asc`)
+            res.json({isError:false,errMess:null,data:messages});
         } catch (error) {
             errorHandler({status:error.status,route:req.path,errMess:error.message})
             res.json({isError:true,errMess:error.message})
--- public/messageForm.js
+++ public/messageForm.js
@@ -1,26 +1,18 @@
+let messageForm = document.querySelector("#messageForm")
+messageForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    console.log('submit clicked')
 
-async function submitMessage() {
-    const messageForm = document.querySelector("#messageForm")
-    messageForm.addEventListener('submit', async (e) => {
-        e.preventDefault()
+    //const message = e.target
+    const messageData = new FormData(messageForm)
+    //messageData.append('message', message.message.value)
+    //messageData.append('messagePic', message.messagePic.files[0])
 
-        //console.log('submit clicked')
-
-        const message = e.target
-        const messageData = new FormData()
-        messageData.append('message', message.message.value)
-        messageData.append('messagePic', message.messagePic.files[0])
-
-        const res = await fetch('/comment', {
-            method: "POST",
-            body: messageData
-        })
-        const result = await res.json()
-        console.log(result)
-        messageForm.reset()
+    const res = await fetch('message/addMessage', {
+        method: "POST",
+        body: messageData
     })
-}
-async function messageMain() {
-    await submitMessage()
-}
-messageMain()
+    const result = await res.json()
+    console.log(result)
+    await messageForm.reset()
+})
